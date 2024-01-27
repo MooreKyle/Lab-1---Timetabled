5e0f0458bbd8a47ffaf0808fffafe1c64eed787f
MooreKyle <moorekl@protonmail.com>
2024-01-26 21:25:36 -0500

    Step 2: Create the Calendar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Calendar from './components/Calendar'; // Imports the 'Calendar' component
 
 const App = () => {
 
@@ -6,9 +7,9 @@ const App = () => {
     <div className="App">
       <h1>Itinerary for 🍀 Ireland 🍀</h1> {/* Replace with your project title */}
       <h2>Welcome to Ireland, Kyle! Check out this calendar to get to know the island and see all the sights during your stay.</h2> {/* Replace with your schedule message */}
-      {/* Additional components and JSX elements will go here */}
+      <Calendar /> {/* The Calendar component goes here */} {/* Additional components and JSX elements will go here */}
     </div>
-  )
+  );
 }
 
 export default App;
